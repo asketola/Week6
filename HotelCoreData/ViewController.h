f3efd2ab8dbf0111de958b5aec6288a96ac99f6e
Anne <star7anne@yahoo.com>
2015-02-09 09:42:01 -0800

    Initial Commit

---
+++ HotelCoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HotelCoreData
+//
+//  Created by Annemarie Ketola on 2/9/15.
+//  Copyright (c) 2015 Up Early, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
